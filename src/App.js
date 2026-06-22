2ffb1165afcb07cb8a5c685fbe87cf1484401503
eCommerce proyectos <equantumg@gmail.com>
2026-06-22 01:53:25 -0300

    Fix: Update routing to remove /Architecture-Engineer-Portfolio-Website paths for Vercel

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/Architecture-Engineer-Portfolio-Website' element={<FullPage />} />
-          <Route path='/Architecture-Engineer-Portfolio-Website/project/:name' element={<ProjectDetails />} />
-        </Routes>
+              <Route exact path='/' element={<FullPage />} />
+          <Route path='/project/:name' element={<ProjectDetails />} />
       </Router>
     </div>
   );
