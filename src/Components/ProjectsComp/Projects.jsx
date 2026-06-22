b84a84a791d0e33746cc268f380a17fddc2bb85d
eCommerce proyectos <equantumg@gmail.com>
2026-06-22 14:35:38 -0300

    Update Projects.jsx

--- src/Components/ProjectsComp/Projects.jsx
+++ src/Components/ProjectsComp/Projects.jsx
@@ -1,136 +1,54 @@
-import React, { useState } from 'react'
-import './EDEX.css'
-import { eddata, exdata } from '../../EDEXData'
+import React from 'react'
+import './SR.css'
+import { Skills } from '../../SR'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 import { motion } from 'framer-motion'
+import Resume from '../../Assets/Resume/Resume.pdf'
 
-const EDEX = () => {
-  const [appear, setAppear] = useState(false)
-  const [secAppear, setSecAppear] = useState(false)
-  const [activeate, SetActivate] = useState('')
-  const [secActiveate, setSecActivate] = useState('active')
-  const [showMore, setShowMore] = useState(false)
-  const [height, setHeight] = useState('')
-  const [showText, setShowText] = useState('Show More')
-
-  const transition = { type: 'spring', duration: 3 }
-
-  const appearanceHandle = () => {
-    if (!secAppear) {
-      setAppear(false)
-      setSecAppear(true)
-      setSecActivate('active')
-      SetActivate('')
-    }
-  }
-
-  const secAppearanceHandle = () => {
-    if (!appear) {
-      setAppear(true)
-      setSecAppear(false)
-      SetActivate('active')
-      setSecActivate('')
-    }
-  }
-
-  const showFullHeight = () => {
-    if (!showMore) {
-      setShowMore(true)
-      setHeight('height')
-      setShowText('Show Less')
-    } else {
-      setShowMore(false)
-      setHeight('')
-      setShowText('Show More')
-    }
-  }
-
+const SR = () => {
   return (
-    <div className={`pad edex ${height}`}>
-      <div className="responsiveBox">
-        <svg onClick={secAppearanceHandle} className='icon res bi bi-mortarboard-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z" />
-          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z" />
-        </svg>
-        <svg onClick={appearanceHandle} className='icon res bi bi-briefcase-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
-          <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
-        </svg>
-      </div>
-      <div className={`left ${activeate}`}>
-        {
-          eddata.map((ed, i) => {
-            return (
-              <motion.div
-                initial={{ transform: 'translateY(100%)', opacity: 0 }}
-                whileInView={{ transform: 'translateY(0%)', opacity: 1 }}
-                transition={{ ...transition }}
-                className='education' key={i}>
-                <p className='text'>{ed.education}</p>
-                <p className='sub'>{ed.sub}</p>
-                <p className='date'>{ed.date}</p>
-              </motion.div>
-            )
-          })
-        }
-      </div>
-      <div className="divider">
-        <motion.svg
-          initial={{ transform: 'rotateY(-90deg)' }}
-          whileInView={{ transform: 'rotateY(0deg)' }}
-          transition={{ ...transition }}
-          className='icon bi bi-briefcase-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
-          <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
-        </motion.svg>
-        <motion.svg
-          initial={{ transform: 'translateX(-100%) rotateY(-90deg)' }}
-          whileInView={{ transform: 'translateX(-100%) rotateY(0deg)' }}
-          transition={{ ...transition }}
-          className='icon bi bi-mortarboard-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z" />
-          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z" />
-        </motion.svg>
-        <div className="circles">
-          {
-            exdata.map((ed, i) => {
+    <>
+      <div className="sr pad space">
+        <div className="left">
+          <ul className='skills'>
+            {Skills.map((skill, i) => {
               return (
-                <motion.div
-                  initial={{ transform: 'translateY(100%) translateX(-50%)', opacity: 0 }}
-                  whileInView={{ transform: 'translateY(0%) translateX(-50%)', opacity: 1 }}
-                  transition={{ ...transition }}
-                  className="circle" key={i}></motion.div>
+                <li key={i}>
+                  <h4>{skill.name}</h4>
+                  <CircularProgressbar value={skill.percent} text={`${skill.percent}%`} styles={buildStyles({
+                    pathColor: '#2c2724',
+                    textColor: '#2c2724',
+                    pathTransitionDuration: 1,
+                  })} />
+                </li>
               )
-            })
-          }
+            })}
+          </ul>
+          <motion.svg
+            initial={{ left: '-7rem', bottom: '-4rem' }}
+            whileInView={{ left: '5rem', bottom: '7rem' }}
+            transition={{ type: 'spring', duration: 1.4 }}
+            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-tools" viewBox="0 0 16 16">
+            <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z" />
+            </motion.svg>
+        </div>
+        <div className="right">
+          <h3>If you find that interesting</h3>
+          <a href={Resume} download="Resume" target='_blank' rel='noreferrer'>
+            <button className='btn'>Download My Resume</button>
+          </a>
+          <motion.svg
+            initial={{ right: '-7rem', bottom: '-4rem' }}
+            whileInView={{ right: '9rem', bottom: '7rem' }}
+            transition={{ type: 'spring', duration: 1.4 }}
+            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-text-fill" viewBox="0 0 16 16">
+            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
+            </motion.svg>
         </div>
       </div>
-      <div className={`right ${secActiveate}`}>
-        {
-          exdata.map((ex, i) => {
-            return (
-              <motion.div
-                initial={{ transform: 'translateY(100%)', opacity: 0 }}
-                whileInView={{ transform: 'translateY(0%)', opacity: 1 }}
-                transition={{ ...transition }}
-                className='experience' key={i}>
-                <p className='text'>{ex.experience}</p>
-                <p className='sub'>{ex.sub}</p>
-                <p className='date'>{ex.date}</p>
-              </motion.div>
-            )
-          })
-        }
-      </div>
-      <motion.div
-        initial={{ opacity: 0.3 }}
-        whileHover={{ opacity: 1 }}
-        transition={{ ...transition }}
-        onClick={showFullHeight}
-        className="show">
-        <p>{showText}</p>
-      </motion.div>
-    </div>
+    </>
   )
 }
 
-export default EDEX
+export default SR
