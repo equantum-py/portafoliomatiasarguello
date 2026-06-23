75d7e08dccbeff8949e86c0493b7de2c3cdbd82c
eCommerce proyectos <equantumg@gmail.com>
2026-06-22 21:40:29 -0300

    Fix JSX syntax errors in Navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => (
-        <nav className="navbar">
-            <div className="navbar-container">
-                  <div className="navbar-logo">ALEX COLOR PY</div>div>
-                  <div className="nav-menu">
-                          <a href="/" className="nav-link">Inicio</a>a>
-                          <a href="#servicios" className="nav-link">Servicios</a>a>
-                          <a href="#experiencia" className="nav-link">Experiencia</a>a>
-                          <a href="#contacto" className="nav-link">Contacto</a>a>
-                  </div>div>
-            </div>div>
-        </nav>nav>
-      );
+          <nav className="navbar">
+              <div className="navbar-container">
+                    <div className="navbar-logo">ALEX COLOR PY</div>div>
+                    <div className="nav-menu">
+                            <a href="/" className="nav-link">Inicio</a>a>
+                            <a href="#servicios" className="nav-link">Servicios</a>a>
+                            <a href="#experiencia" className="nav-link">Experiencia</a>a>
+                            <a href="#contacto" className="nav-link">Contacto</a>a>
+                    </div>div>
+              </div>div>
+          </nav>nav>
+        );
 
 export default Navbar;</nav>
